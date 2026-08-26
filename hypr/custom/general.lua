3eb7818115517f5889e25898aae817af255a52cb
jeffersonmk <jeffersonmk@proton.me>
2026-08-26 12:32:32 -0300

    first commit

---
+++ hypr/custom/general.lua
@@ -0,0 +1,8 @@
+hl.config({
+    input = {
+        -- 1. REMOVER ACELERAÇÃO DO MOUSE
+        force_no_accel = true,
+        accel_profile = "flat",
+        sensitivity = 0.0, -- Deixe em 0.0 para input puro (ajuste se precisar)
+    }
+})
---
+++ hypr/custom/keybinds.lua
@@ -0,0 +1,26 @@
+
+-- User
+
+hl.bind(
+    "CTRL+SUPER+C",
+    hl.dsp.exec_cmd("helium-browser"),
+    { description = "Helium" }
+)
+
+hl.bind(
+    "CTRL+SUPER+O",
+    hl.dsp.exec_cmd("obsidian"),
+    { description = "Obsidian" }
+)
+
+hl.bind(
+    "CTRL+SUPER+G",
+    hl.dsp.exec_cmd("cartridges"),
+    { description = "Cartridges" }
+)
+
+hl.bind(
+    "CTRL+SUPER+D",
+    hl.dsp.exec_cmd("vesktop"),
+    { description = "Vesktop (Discord Client)" }
+)
